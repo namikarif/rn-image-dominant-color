9adda2f370727070dea68322acfaf3b369013846
Namig <45629136+namikarif@users.noreply.github.com>
2023-05-19 14:50:06 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,60 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import { Canvas, Image } from 'react-native-canvas';
+import ImageSize from 'react-native-image-size';
+
+export async function getDominantColor(imagePath: string) {
+  return new Promise<string>(async (resolve, reject) => {
+    try {
+      const size = await ImageSize.getSize(imagePath);
+      const { width, height } = size;
+
+      getPixelData(width, height, imagePath, (dominantColor: string) => {
+        resolve(dominantColor);
+      });
+    } catch (error) {
+      reject('Resim boyutu alınamadı: ' + error);
+    }
+  });
 }
+
+const getPixelData = (
+  width: number,
+  height: number,
+  imagePath: string,
+  returnDominantColor: Function,
+) => {
+  let canvas = new Canvas(width, height);
+  const ctx = canvas.getContext('2d');
+
+  const image = new Image(canvas, width, height);
+  image.src = imagePath;
+
+  image.addEventListener('load', () => {
+    ctx.drawImage(image, 0, 0);
+    const imageData = ctx.getImageData(0, 0, width, height);
+
+    returnDominantColor(findDominantColor(imageData.data));
+  });
+
+  image.addEventListener('loadstart', () => {
+    ctx.drawImage(image, 0, 0);
+  });
+};
+
+const findDominantColor = (pixelData: any) => {
+  let totalRed = 0;
+  let totalGreen = 0;
+  let totalBlue = 0;
+
+  for (let i = 0; i < pixelData.length; i += 4) {
+    totalRed += pixelData[i];
+    totalGreen += pixelData[i + 1];
+    totalBlue += pixelData[i + 2];
+  }
+
+  const pixelCount = pixelData.length / 4;
+  const dominantRed = Math.round(totalRed / pixelCount);
+  const dominantGreen = Math.round(totalGreen / pixelCount);
+  const dominantBlue = Math.round(totalBlue / pixelCount);
+
+  return `rgb(${dominantRed}, ${dominantGreen}, ${dominantBlue})`;
+};
